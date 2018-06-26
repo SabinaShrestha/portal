487ea7e2ae94171b03e3ff80993c17a1d9deb5b2
David Jungst <djungst@gmail.com>
2018-06-26 17:05:32 -0600

    Permission component for wrapping content
    Summary:
    Use:
    <Permission enrollment={} type="staff">
      <MyProtectedStuff />
    </Permison>
    
    Reviewers: #portal2, MichelleGarcia
    
    Reviewed By: #portal2, MichelleGarcia
    
    Differential Revision: http://phabricator.devpointstudios.com/D2178

---
+++ client/src/components/hoc/Permission.js
@@ -0,0 +1,31 @@
+import { Component } from 'react'
+import { connect } from 'connect'
+
+class Permission extends Component {
+  teacher = () => {
+    const { enrollment: { role }, children } = this.props
+    return role === 'teacher' ? children : null 
+  }
+
+  staff = () => {
+    const { enrollment: { role }, children } = this.props
+    const regex = new RegExp(/(teacher|ta)/)
+    return regex.test(role) ? children : null
+  }
+
+  render() {
+    const { admin, type, children } = this.props
+    if (admin)
+      return children
+
+    const func = this[type]
+    func()
+  }
+}
+
+const mapStateToProps = (state, props) => {
+  const enrollment = props.enrollment || state.enrollment || {}
+  return { admin: user.is_admin, enrollment }
+}
+
+export connect(mapStateToProps)(Permission)
