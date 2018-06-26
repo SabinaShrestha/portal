853136c9d131c3c56838163673ff908d486880c5
David Jungst <djungst@gmail.com>
2018-06-26 17:29:53 -0600

    Move assignments spec
    Reviewers: #portal2, MichelleGarcia
    
    Reviewed By: #portal2, MichelleGarcia
    
    Differential Revision: http://phabricator.devpointstudios.com/D2182

--- spec/controllers/api/assignments_controller_spec.rb
+++ spec/controllers/api/assignments_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe AssignmentsController, type: :controller do
+RSpec.describe Api::AssignmentsController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
