3c297ae7bbe642523a408a2b90c3c46520f857bf
Pete Sword <pete.sword@yahoo.com>
2018-06-26 17:28:17 -0600

    group testing
    Summary: group model spec
    
    Reviewers: #portal2, migobach
    
    Reviewed By: #portal2, migobach
    
    Differential Revision: http://phabricator.devpointstudios.com/D2181

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,2 +1,29 @@
 require 'rails_helper'
 
+RSpec.describe Group, type: :model do
+  
+  it { should belong_to(:course) }
+  
+  describe 'attributes' do 
+    it 'has a name' do
+      name = 'Test Group'
+      group = Group.create(name: name)
+      expect(group.name).to eq(name)
+    end
+  end
+
+  describe 'associations' do
+    it { should belong_to(:course) }
+    it { should have_many(:group_memberships) }
+    it { should have_many(:enrollments).through(:group_memberships) }
+  end
+
+  describe 'validations' do
+    context 'validate' do
+      it { should validate_presence_of(:name) }
+    end
+  end
+
+end
+
+
