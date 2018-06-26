cb9eb45a363fd3d8ec8eaae60ffaf2feb0910861
David Jungst <djungst@gmail.com>
2018-06-26 16:19:39 -0600

    add theme
    Reviewers: #portal2, brittaniepham, awinitzky
    
    Reviewed By: #portal2, brittaniepham, awinitzky
    
    Subscribers: awinitzky
    
    Differential Revision: http://phabricator.devpointstudios.com/D2166

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
 import store from './store'
 import 'semantic-ui-css/semantic.min.css'
+
+const theme = {
+  primary: '#9575CD',
+  secondary: '#614D7B',
+}
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
