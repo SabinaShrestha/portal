2650580e247f46f94756eb4c9e90db6a94b7d5c7
David Jungst <djungst@gmail.com>
2018-06-26 17:25:18 -0600

    move quizzes controller
    Reviewers: #portal2, MichelleGarcia
    
    Reviewed By: #portal2, MichelleGarcia
    
    Differential Revision: http://phabricator.devpointstudios.com/D2180

--- spec/controllers/api/quizzes_controller_spec.rb
+++ spec/controllers/api/quizzes_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe QuizzesController, type: :controller do
+RSpec.describe Api::QuizzesController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
