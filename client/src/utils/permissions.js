121ec8d44f93ca6110810c70574c4e851aa391a2
Alex Winitzky <alex.winitzky@gmail.com>
2018-06-27 14:49:32 -0600

    remove register link from navBarALSO, sorry working on master.
    Summary: add user permissions functions
    
    Reviewers: #portal2, sariasong
    
    Reviewed By: #portal2, sariasong
    
    Subscribers: sariasong
    
    Differential Revision: http://phabricator.devpointstudios.com/D2210

---
+++ client/src/utils/permissions.js
@@ -0,0 +1,24 @@
+export const isAdmin = (currentUser) => {
+  return currentUser.is_admin
+}
+
+export const isStaff = (currentUser) => {
+  let regex = new RegExp('(^ta$|^teacher$)')
+  return currentUser.is_admin || regex.test(currentUser.role);
+}
+
+export const isTeacher = (currentUser) => {
+  return currentUser.role === 'teacher' ? true : false
+}
+
+export const isTa = (currentUser) => {
+  return currentUser.role === 'ta' ? true : false
+}
+
+export const isStudent = (currentUser) => {
+  return currentUser.role === 'student' ? true : false
+}
+
+export const isTeacherOrAdmin = (currentUser) => {
+  return currentUser.role === 'teacher' || currentUser.is_admin
+}
